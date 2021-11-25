bd65dd1bd9210727bc7e47e9121484a6ad5ef791
rabbithay <thay.csb@gmail.com>
2021-11-25 01:21:12 -0300

    fix: logout  button

--- src/components/ExitButton.jsx
+++ src/components/ExitButton.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { ImExit } from 'react-icons/im';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import useAuthConfig from '../hooks/authConfig';
 import handleError from '../hooks/handleError';
+import UserContext from '../contexts/UserContext';
 
 export default function ExitButton() {
   const history = useHistory();
   const config = useAuthConfig();
+  const { setUserInfo } = useContext(UserContext);
 
   function logOut() {
-    axios.post('http://localhost:4002/exit', {}, config).then(() => {
+    axios.post('http://localhost:4002/logout', {}, config).then(() => {
+      setUserInfo('');
       history.push('/');
     }).catch((error) => {
       handleError(error);
