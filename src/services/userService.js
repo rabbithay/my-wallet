5a8002b97c44eaf9232100f635fa66ad20aa0544
rabbithay <thay.csb@gmail.com>
2021-11-26 02:15:54 -0300

    config: environment variables

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:4002'
-  : 'https://myywallet.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export function register(body) {
   return axios.post(`${BASE_URL}/register`, body);
